3eb0d53e0459ee7192fc187bd6ca5f314128e430
Felipe Ferreira <ferreiraassis98@gmail.com>
2018-08-17 15:53:04 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/example/democom/felipe/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.example.democom.felipe.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.democom.felipe.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
--- src/main/java/com/example/democom/felipe/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/example/democom/felipe/cursomc/resources/CategoriaResource.java
@@ -3,28 +3,38 @@ package com.example.democom.felipe.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.democom.felipe.cursomc.domain.Categoria;
+import com.example.democom.felipe.cursomc.services.CategoriaServices;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET) //salvar é POST, deletar DELET, etc...
-	public List<Categoria> Listar() {
+	@Autowired
+	private CategoriaServices service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) //salvar é POST, deletar DELET, etc...
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 		
-		Categoria cat1 = new Categoria(1, "informatica");
-		Categoria cat2 = new Categoria(2, "escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+//				LISTA DEIXOU DE EXISTIR, AGORA É O find() .. Esse metodo abaixo foi mostrado pelo professor
+//		Categoria cat1 = new Categoria(1, "informatica");
+//		Categoria cat2 = new Categoria(2, "escritório");
+//		
+//		List<Categoria> lista = new ArrayList<>();
+//		
+//		lista.add(cat1);
+//		lista.add(cat2);
+//		
+//		return lista;
 	}
 	
 }
