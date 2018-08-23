6721e7d68fc22ee5c65f472de934b0ba6adae932
Felipe Ferreira <ferreiraassis98@gmail.com>
2018-08-23 20:28:06 -0300

    Inserindo uma nova categoria com POST

--- src/main/java/com/example/democom/felipe/cursomc/services/CategoriaServices.java
+++ src/main/java/com/example/democom/felipe/cursomc/services/CategoriaServices.java
@@ -19,5 +19,10 @@ public class CategoriaServices {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id
 					+ ", tipo: " + Categoria.class.getName()));
-	}	
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
