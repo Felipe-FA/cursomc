684a31be5c90c7ffa0063241340d79d9e3792d21
Felipe Ferreira <ferreiraassis98@gmail.com>
2018-08-27 15:17:04 -0300

    Correcao do commit passado

---
+++ src/main/java/com/example/democom/felipe/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,49 @@
+package com.example.democom.felipe.cursomc.services.validation;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.democom.felipe.cursomc.domain.Cliente;
+import com.example.democom.felipe.cursomc.domain.enums.TipoCliente;
+import com.example.democom.felipe.cursomc.dto.ClienteNewDTO;
+import com.example.democom.felipe.cursomc.repositories.ClienteRepository;
+import com.example.democom.felipe.cursomc.resources.exception.FieldMessage;
+import com.example.democom.felipe.cursomc.services.validation.utils.BR;
+
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
